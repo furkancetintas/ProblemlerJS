34c67ae8d1a70f9936a56f7ac3b9e3f227b9e59b
furkancetintas <furkancetintas01@gmail.com>
2020-06-06 13:46:40 +0300

    LeetCode Problem 27

---
+++ LeetCodeProblem27.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let len = nums.length
+    for (let i = 0; i < len; i++) {
+        if (nums[i] === val) {
+            nums[i--] = nums[--len]
+        }
+    }
+    return len
+};
